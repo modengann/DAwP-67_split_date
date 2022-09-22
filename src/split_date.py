2b4c7e9e5fd3c5e71a488f278be16755070bac89
modengann <99417989+modengann@users.noreply.github.com>
2022-09-22 10:09:40 -0400

    init

---
+++ src/split_date.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+from os import sep
+import pandas as pd
+import numpy as np
+
+
+def split_date():
+    pass
+
+def main():
+    split_date()
+       
+if __name__ == "__main__":
+    main()
